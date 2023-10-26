039273bb06c082e487c64776545dbe5d7b276da8
Diego Castillo <diegocasmo@gmail.com>
2023-10-26 08:43:42 -0500

    Add fix grammar script

---
+++ raycast/fix-grammar-errors-chatgpt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title fix-grammar-errors-chatgpt
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Diego Castillo
+
+
+echo -n "Fix the grammar errors in this text only. Do not reword.
+
+\`\`\` md
+\`\`\`" | pbcopy
+
